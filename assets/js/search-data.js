ceb63ac199abf2474d61bc4eac6c9c4b52641921
raddatzk <kevin.raddatz@valtech-mobility.com>
2025-03-19 12:44:17 +0000

    Deploying to gh-pages from @ raddatzk/raddatzk.github.io@a9ae7221ab9c44900da09e2fbc8b5208408cbc54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,11 +9,55 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-personal",
-          title: "personal",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/personal";
-          },
-        },];
+  },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/private/blog";
+              },
+            },{id: "post-2024-teneriffa-2",
+      
+        title: "2024 | Teneriffa 2",
+      
+      description: "Explore the adventurous side of tenerife",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/private/2025/03/13/teneriffa-2.html";
+        
+      },
+    },{id: "post-2024-teneriffa",
+      
+        title: "2024 | Teneriffa",
+      
+      description: "Explore the adventurous side of tenerife",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/private/2025/03/13/teneriffa.html";
+        
+      },
+    },{id: "post-a-post-with-formatting-and-links-2",
+      
+        title: "a post with formatting and links 2",
+      
+      description: "march &amp; april, looking forward to summer",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/professional/2015/03/15/formatting-and-links2.html";
+        
+      },
+    },{id: "post-a-post-with-formatting-and-links",
+      
+        title: "a post with formatting and links",
+      
+      description: "march &amp; april, looking forward to summer",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/professional/2015/03/15/formatting-and-links.html";
+        
+      },
+    },];
