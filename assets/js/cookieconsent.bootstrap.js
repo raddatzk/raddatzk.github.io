bc4a88caa48c58e7aad777f141ccfe241bd643ac
raddatzk <kevin.raddatz@valtech-mobility.com>
2025-04-02 14:06:06 +0000

    Deploying to gh-pages from @ raddatzk/raddatzk.github.io@68d312e50703bb5180a5e397930ce669ca6c9e9b 🚀

--- assets/js/cookieconsent.bootstrap.js
+++ assets/js/cookieconsent.bootstrap.js
@@ -10,12 +10,11 @@ var ccb =
 
 ccb_youtube: {
     title: "YouTube",
-        description: "YouTube: Enable embedding YouTube videos",
+        description: "YouTube videos are integrated on this website",
+        action: "Embed YouTube videos in this website",
         disabled: false,
     allowed: false,
-    expirydays: 365,
-    loaded: false,
-        iframe_placeholder: ""
+    loaded: false
 },
 
 },
@@ -28,28 +27,28 @@ strings: {
         btnMore: "More",
 },
 
-onfirstload: function () {
+onFirstLoad: function () {
     if (!ccb.setupcomplete) {
         if (!(window.jQuery)) {
             ccb.jqueryattempts++;
             if (ccb.jqueryattempts >= 5) {
                 return;
             }
-            setTimeout(ccb.onfirstload, 200);
+            setTimeout(ccb.onFirstLoad, 200);
             return;
         }
         ccb.setupcomplete = true;
     }
-    ccb.checkapproval();
+    ccb.checkApproval();
 },
 initialise: function (obj) {
     if (obj.show_banner !== undefined) {
         ccb.show_banner = obj.show_banner;
     }
     // check if jquery has been loaded
-    ccb.onfirstload();
+    ccb.onFirstLoad();
 },
-checkapproval: function () {
+checkApproval: function () {
     ccb.readCookies();
     if (!ccb.consent_approved) {
         if (ccb.show_banner) {
@@ -60,22 +59,10 @@ checkapproval: function () {
     }
 },
 readCookies: function () {
-    var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        x = x.replace(/^\s+|\s+$/g, "");
-        cookieval =  unescape(y);
-        if (cookieval) {
-            if (x in ccb.cookies) {
-                if (cookieval == "true") {
-                    ccb.cookies[x].allowed = true;
-                } else {
-                    ccb.cookies[x].allowed = false;
-                }
-                ccb.cookies[x].loaded = true;
-            }
-        }
+    for (let key of Object.keys(ccb.cookies)) {
+        let value = localStorage.getItem(key);
+        ccb.cookies[key].allowed = value === "true";
+        ccb.cookies[key].loaded = true;
     }
     ccb.consent_approved = true;
     jQuery.each(ccb.cookies, function (key, value) {
@@ -84,23 +71,19 @@ readCookies: function () {
         }
     });
 },
-setcookie: function (name, value, expirydays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + expirydays);
-    document.cookie = name + '=' + value + '; expires=' + exdate.toUTCString() + '; path=/' + '; SameSite=Strict'
+setCookie: function (name, value) {
+    localStorage.setItem(name, value);
 },
 showBanner: function () {
     jQuery('#ccb-notification').remove();
-    if (ccb.style == "dark") {
+    if (determineComputedTheme() === "dark") {
         background_color = "bg-dark";
         text_color = "text-white";
-        btn_select_color = "btn-outline-light";
     } else {
         background_color = "bg-light";
         text_color = "text-black";
-        btn_select_color = "btn-outline-dark";
     }
-    data = `
+    let data = `
           <div class="modal fade show" id="cookieDialog" role="dialog" data-backdrop="static">
             <div class="modal-dialog modal-lg" role="document">
               <div class="modal-content ${background_color} ${text_color}">
@@ -109,17 +92,24 @@ showBanner: function () {
                 </div>
                 <div class="modal-body">
                   <p><div id="ccb-notification-text"></div></p>
-                  <div id="ccb-cookie-list" class="d-flex flex-row mb-1"></div>
-                  <div class="collapse" id="collapseCookieDetails">
-                    <div class="d-flex flex-column mb-1" id="ccb-cookie-details">
+                  
+                    <div class="d-flex flex-row mb-1">
+                      <div class="pr-1">
+                        <label class="switch">
+                          <input type="checkbox" id="ccb-toggle-ccb_youtube">
+                          <span class="slider round"></span>
+                        </label>
+                      </div>    
+                      <div class="pl-2 pr-5">YouTube</div>
+                      <div class="pl-2 pr-5">Embed YouTube videos in this website</div>
                     </div>
-                  </div>
+                  
                 </div>
                 <div class="modal-footer d-flex flex-column">
                   <button type="button" class="btn btn-success mt-3 btn-lg btn-block" id="ccb-btnAcceptAll">
                     Accept All
                   </button>
-                   <button type="button" class="btn ${btn_select_color} mt-3 btn-lg btn-block" id="ccb-btnAcceptSelected">
+                   <button type="button" class="btn mt-3 btn-lg btn-block" id="ccb-btnAcceptSelected">
                     Accept only selected
                   </button>
                 </div>
@@ -151,38 +141,16 @@ showBanner: function () {
         ccb.closeBanner();
     });
     jQuery.each(ccb.cookies, function (key, value) {
-        data_cookie = `
-            <div class="pr-1">
-              <label class="switch">
-                <input type="checkbox" id="ccb-toggle-${key}">
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div class="pl-2 pr-5">${ccb.cookies[key].title}</div>
-          `;
-        data_more = `
-            <div class="mt-2">${ccb.cookies[key].description}</div>
-          `;
-        jQuery('#ccb-cookie-list').append(data_cookie);
-        jQuery('#ccb-cookie-details').append(data_more);
         jQuery(`#ccb-toggle-${key}`).attr('checked', ccb.cookies[key].allowed);
         jQuery(`#ccb-toggle-${key}`).attr('disabled', ccb.cookies[key].disabled);
     });
-    data_more = `
-          <div>
-            <a data-toggle="collapse" href="#collapseCookieDetails" role="button" aria-expanded="false" aria-controls="collapseCookieDetails">
-              ${ccb.strings.btnMore}...
-            </a>
-          </div>
-        `;
-    jQuery('#ccb-cookie-list').append(data_more);
     $('#cookieDialog').modal('show')
 },
 closeBanner: function () {
     $('#cookieDialog').modal('hide')
     ccb.consent_approved = true;
     jQuery.each(ccb.cookies, function (key, value) {
-        ccb.setcookie(key, ccb.cookies[key].allowed, ccb.cookies[key].expirydays);
+        ccb.setCookie(key, ccb.cookies[key].allowed);
     });
     ccb.executeScripts();
 },
@@ -198,22 +166,26 @@ executeScripts: function () {
             jQuery(this).remove();
         }
     });
-    jQuery('iframe.ccb-cookie-consent').each(function () {
+    jQuery('.cookie-wrapper').each(function () {
         key = jQuery(this).attr("ccb-cookie-type");
+        jQuery(this).empty()
         if (ccb.cookies[key].allowed) {
-            jQuery(this).attr("src", jQuery(this).attr("data-src"));
+            switch(key) {
+                case "ccb_youtube":
+                    let youtube = jQuery(this).attr("youtube")
+                    jQuery(this).append('<iframe src="https://www.youtube.com/embed/' + youtube + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+            }
         } else {
-            jQuery(this).after(`<div class="youtube-placeholder">
-    <svg xmlns="http://www.w3.org/2000/svg" width="900" height="506.25" viewBox="0 0 180.119 139.794" fill="currentColor">
-        <g transform="translate(-47.59 -66.639)" paint-order="fill markers stroke">
-            <path d="M13.591 66.639H293.71v139.794H13.591z"/>
-            <path d="m152.507 133.514-34.249 34.249-15.968-15.968-41.938 41.937H212.726z" opacity=".675" fill="#fff"/>
-            <circle cx="92.217" cy="108.555" r="11.773" opacity=".675" fill="#fff"/>
-            <path fill="none" d="M26.111 77.634h152.614v116.099H26.111z"/>
+            jQuery(this).append(`<div class="youtube-placeholder">
+    <svg onclick="ccb.showBanner()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 506.25" fill="currentColor">
+        <g paint-order="fill markers stroke">
+            <path d="M0 0 H900 v506.25 H0 z"></path>
+            <circle cx="270" cy="100" r="40" opacity=".675" fill="#fff"></circle>
+            <path d="m100 400 195 -195 85 85 164 -164 256 274 z" opacity=".675" fill="#fff"></path>
+            <text x="248" y="475" fill="#fff" opacity=".675" font-size="xx-large">Click here to show cookie banner</text>
         </g>
     </svg>
 </div>`);
-            jQuery(this).remove();
         }
     });
 },
